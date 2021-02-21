98aa34a00a06475117586960cc8e0d30241898b5
Onno Broekmans <o.broekmans@lumicks.com>
2021-02-21 12:47:34 +0100

    event-session-signup: customize
    Small improvements and fixes to the event-session-signup example. Includes:
    
    - Can run "Set up conference" multiple times; each time it's run, the calendar and form are updated instead of recreated. Only limitation is that calendar appointments are not deleted when sessions are removed; this is intentional.
    - Add menu options for opening the associated form & calendar.
    - Add menu option for "resetting", so that the spreadsheet forgets about the linked form and calendar. This brings the spreadsheet back into an "initial", clean state, from where a new calendar & form can be created again.
    - When adding guests to an event, trigger a Google Calendar notification email to the guest, instead of sending an "itinerary" email. This requires the Google Calendar Advanced API.
    - Automatically create Hangout/Meet links for each event. Again, this requires the Google Calendar Advanced API.

--- event-session-signup/src/Code.js
+++ event-session-signup/src/Code.js
@@ -17,7 +17,12 @@
  */
 function onOpen() {
   SpreadsheetApp.getUi().createMenu('Conference')
-      .addItem('Set up conference', 'setUpConference_')
+      .addItem('Set up conference', 'setUpConference')
+      .addItem('Open registration form', 'openForm_')
+      .addItem('Open calendar', 'openCalendar_')
+      .addSubMenu(SpreadsheetApp.getUi().createMenu('Advanced')
+        .addItem('Reset conference', 'resetConference_')
+      )
       .addToUi();
 }
 
@@ -25,20 +30,41 @@ function onOpen() {
  * A set-up function that uses the conference data in the spreadsheet to create
  * Google Calendar events, a Google Form, and a trigger that allows the script
  * to react to form responses.
+ * This function can also be run to update an existing calendar/form with 
+ * modified sessions.
  */
-function setUpConference_() {
-  if (ScriptProperties.getProperty('calId')) {
-    Browser.msgBox('Your conference is already set up. Look in Google Drive for your'
-                   + ' sign up form!');
-  }
+function setUpConference() {
   var ss = SpreadsheetApp.getActive();
   var sheet = ss.getSheetByName('Conference Setup');
   var range = sheet.getDataRange();
   var values = range.getValues();
   setUpCalendar_(values, range);
   setUpForm_(ss, values);
-  ScriptApp.newTrigger('onFormSubmit').forSpreadsheet(ss).onFormSubmit()
-      .create();
+}
+
+/** 
+ * Reset our "memory", so that next time we'll create a new calendar and registration
+ * form.
+ */
+function resetConference_() {
+  var ui = SpreadsheetApp.getUi();
+  var response = ui.alert(
+    'Are you sure you want to reset the conference registration? ' +
+    'This will unlink any existing calendar and form, and cause a new ' +
+    'calendar and form to be created next time you use "Set up conference".',
+    ui.ButtonSet.OK_CANCEL);
+  if (response == ui.Button.CANCEL) {
+    return;
+  }
+
+  PropertiesService.getScriptProperties().deleteProperty('calendarId');
+  PropertiesService.getScriptProperties().deleteProperty('formId');
+  
+  var triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++) {
+    Logger.log("Deleting project trigger...")
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
 }
 
 /**
@@ -48,22 +74,68 @@ function setUpConference_() {
  * @param {Range} range A spreadsheet range that contains conference data.
  */
 function setUpCalendar_(values, range) {
-  var cal = CalendarApp.createCalendar('Conference Calendar');
-  // Start at 1 to skip the header row.
-  for (var i = 1; i < values.length; i++) {
+  var cal = null;
+  var calId = PropertiesService.getScriptProperties().getProperty('calendarId');
+  if ((calId === null) || (CalendarApp.getCalendarById(calId) === null)) {
+    // Need to create new calendar.
+    cal = CalendarApp.createCalendar('LUMICKS Academy Calendar');
+    calId = cal.getId();
+    // Store the ID for the Calendar, which is needed to retrieve events by ID.
+    PropertiesService.getScriptProperties().setProperty('calendarId', calId);
+  }
+  else {
+    // Update existing calendar.
+    cal = CalendarApp.getCalendarById(calId);
+  }
+
+  // NOTE: We do NOT delete any events from the calendar. If a session gets deleted,
+  // the calendar has to be updated manually. (This is intended.)
+  for (var i = 1; i < values.length; i++) {  // start at 1 to skip the sheet's header row
     var session = values[i];
     var title = session[0];
     var start = joinDateAndTime_(session[1], session[2]);
     var end = joinDateAndTime_(session[1], session[3]);
-    var options = {location: session[4], sendInvites: true};
-    var event = cal.createEvent(title, start, end, options)
-        .setGuestsCanSeeGuests(false);
+    var location = session[4];
+    var event = cal.getEventById(session[5]);
+    if (event == null) {
+      // Create new event on calendar. We use this circumvent approach in order to
+      // be able to generate a Hangouts link with it.
+      // See https://stackoverflow.com/a/65216722/1037
+      var eventPayload = {
+        "summary": title, 
+        "start": {"dateTime": start.toISOString()},
+        "end": {"dateTime": end.toISOString()},
+        "location": location,
+        "conferenceData": {
+          "createRequest": {
+            "conferenceSolutionKey": {
+              "type": "hangoutsMeet",
+            }, 
+            "requestId": Utilities.getUuid(),
+          }
+        }
+      };
+      var eventReturn = Calendar.Events.insert(
+        eventPayload, 
+        calId, 
+        {
+          "conferenceDataVersion": 1,
+          "sendUpdates": "all",
+        });
+      event = cal.getEventById(eventReturn.id);
+    } else {
+      // Update event settings to be in sync with sheet data.
+      event.setTitle(title);
+      event.setTime(start, end);
+      event.setLocation(location);
+    }
+
+    event.setGuestsCanSeeGuests(true);
+
+    // Store event ID for future updates.
     session[5] = event.getId();
   }
   range.setValues(values);
-
-  // Store the ID for the Calendar, which is needed to retrieve events by ID.
-  ScriptProperties.setProperty('calId', cal.getId());
 }
 
 /**
@@ -94,8 +166,8 @@ function setUpForm_(ss, values) {
   // Start at 1 to skip the header row.
   for (var i = 1; i < values.length; i++) {
     var session = values[i];
-    var day = session[1].toLocaleDateString();
-    var time = session[2].toLocaleTimeString();
+    var day = session[1].toLocaleDateString('en-GB');
+    var time = session[2].toLocaleTimeString('en-GB');
     if (!schedule[day]) {
       schedule[day] = {};
     }
@@ -106,10 +178,40 @@ function setUpForm_(ss, values) {
   }
 
   // Create the form and add a multiple-choice question for each timeslot.
-  var form = FormApp.create('Conference Form');
+  var form = null;
+  var formId = PropertiesService.getScriptProperties().getProperty('formId');
+  try {
+    if (formId !== null) {
+      form = FormApp.openById(formId);
+    }
+  } catch(e) {
+    form = null;
+  }
+  if (form === null) {
+    // Create new form.
+    form = FormApp.create("LUMICKS Academy Registration");
+    formId = form.getId();
+    PropertiesService.getScriptProperties().setProperty('formId', formId);
+
+    form.addTextItem().setTitle('Name').setRequired(true);
+    form.addTextItem().setTitle('Email').setRequired(true);
+
+    // Run our `onFormSubmit` code whenever the form gets submitted.
+    ScriptApp.newTrigger('onFormSubmit').forSpreadsheet(ss).onFormSubmit()
+        .create();
+  }
+
+  // Create a new empty sheet for receiving registrations. (So each time we run, we create
+  // a fresh sheet, keeping old ones around for reference.)
   form.setDestination(FormApp.DestinationType.SPREADSHEET, ss.getId());
-  form.addTextItem().setTitle('Name').setRequired(true);
-  form.addTextItem().setTitle('Email').setRequired(true);
+
+  // (Re-)create questions for all sessions.
+  form.getItems(FormApp.ItemType.MULTIPLE_CHOICE).forEach(function(item) {
+    form.deleteItem(item);
+  });
+  form.getItems(FormApp.ItemType.SECTION_HEADER).forEach(function(item) {
+    form.deleteItem(item);
+  });
   Object.keys(schedule).forEach(function(day) {
     var header = form.addSectionHeaderItem().setTitle('Sessions for ' + day);
     Object.keys(schedule[day]).forEach(function(time) {
@@ -128,6 +230,8 @@ function setUpForm_(ss, values) {
  */
 function onFormSubmit(e) {
   var user = {name: e.namedValues['Name'][0], email: e.namedValues['Email'][0]};
+  Logger.log(`OnFormSubmit: ${user.name} -- ${user.email}`);
+  Logger.log(e.namedValues);
 
   // Grab the session data again so that we can match it to the user's choices.
   var response = [];
@@ -136,18 +240,18 @@ function onFormSubmit(e) {
   for (var i = 1; i < values.length; i++) {
     var session = values[i];
     var title = session[0];
-    var day = session[1].toLocaleDateString();
-    var time = session[2].toLocaleTimeString();
+    var day = session[1].toLocaleDateString('en-GB');
+    var time = session[2].toLocaleTimeString('en-GB');
     var timeslot = time + ' ' + day;
 
     // For every selection in the response, find the matching timeslot and title
     // in the spreadsheet and add the session data to the response array.
-    if (e.namedValues[timeslot] && e.namedValues[timeslot] == title) {
+    if (e.namedValues[timeslot] && (e.namedValues[timeslot].indexOf(title) !== -1)) {
+      Logger.log(`${user.name} registered for ${session}`)
       response.push(session);
     }
   }
   sendInvites_(user, response);
-  sendDoc_(user, response);
 }
 
 /**
@@ -156,38 +260,57 @@ function onFormSubmit(e) {
  * @param {Array<String[]>} response An array of data for the user's session choices.
  */
 function sendInvites_(user, response) {
-  var id = ScriptProperties.getProperty('calId');
-  var cal = CalendarApp.getCalendarById(id);
+  var calId = ScriptProperties.getProperty('calendarId');
   for (var i = 0; i < response.length; i++) {
-    cal.getEventSeriesById(response[i][5]).addGuest(user.email);
+    addGuestAndSendEmail_(calId, response[i][5], user.email);
   }
 }
 
-/**
- * Create and share a personalized Google Doc that shows the user's itinerary.
- * @param {object} user An object that contains the user's name and email.
- * @param {Array<string[]>} response An array of data for the user's session choices.
- */
-function sendDoc_(user, response) {
-  var doc = DocumentApp.create('Conference Itinerary for ' + user.name)
-      .addEditor(user.email);
-  var body = doc.getBody();
-  var table = [['Session', 'Date', 'Time', 'Location']];
-  for (var i = 0; i < response.length; i++) {
-    table.push([response[i][0], response[i][1].toLocaleDateString(),
-      response[i][2].toLocaleTimeString(), response[i][4]]);
-  }
-  body.insertParagraph(0, doc.getName())
-      .setHeading(DocumentApp.ParagraphHeading.HEADING1);
-  table = body.appendTable(table);
-  table.getRow(0).editAsText().setBold(true);
-  doc.saveAndClose();
+function addGuestAndSendEmail_(calendarId, eventId, newGuest) {
+  Logger.log(`addGuestAndSendEmail(${calendarId}, ${eventId}, ${newGuest})`);
 
-  // Email a link to the Doc as well as a PDF copy.
-  MailApp.sendEmail({
-    to: user.email,
-    subject: doc.getName(),
-    body: 'Thanks for registering! Here\'s your itinerary: ' + doc.getUrl(),
-    attachments: doc.getAs(MimeType.PDF),
-  });
+  // In the Calendar Advanced API, we need to strip the "@google.com" suffix from the
+  // end of the Event ID, or it won't work [0].
+  // [0]: https://stackoverflow.com/a/55509409
+  var calEventId = eventId.replace('@google.com', '');
+  var event = Calendar.Events.get(calendarId, calEventId);
+  Logger.log(event);
+  var attendees = 'attendees' in event ? event.attendees : [];
+  attendees.push({email: newGuest});
+
+  var resource = { attendees: attendees };
+  var args = { sendUpdates: "all" };
+
+  Calendar.Events.patch(resource, calendarId, calEventId, args);
+}
+
+function openUrl_(url) {
+  var js = `<script>window.open('${url}', '_blank'); google.script.host.close();</script>`;
+  var html = HtmlService.createHtmlOutput(js)
+    .setHeight(10)
+    .setWidth(100);
+  SpreadsheetApp.getUi().showModalDialog(html, 'Now loading.')
+}
+
+function openForm_() {
+  var formId = PropertiesService.getScriptProperties().getProperty('formId');
+  if (formId === null) {
+    SpreadsheetApp.getUi().alert('No registration form has been set up yet.');
+    return;
+  }
+
+  var form = FormApp.openById(formId);
+  const formUrl = form.getEditUrl();
+  openUrl_(formUrl);
+}
+
+function openCalendar_() {
+  var calId = PropertiesService.getScriptProperties().getProperty('calendarId');
+  if (calId === null) {
+    SpreadsheetApp.getUi().alert('No calendar has been set up yet.');
+    return;
+  }
+
+  const calUrl = `https://calendar.google.com/calendar/?cid=${calId}`;
+  openUrl_(calUrl);
 }
